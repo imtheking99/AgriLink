7a4ac9de91c98ebc95213fd62170fc541e614ce7
imtheking99 <kumarabp3@gmail.com>
2026-07-04 15:49:37 +0530

    userprofile fet

--- Pages/forms.py
+++ Pages/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import Crop
+from .models import Crop, UserProfile
 
 class CropForm(forms.ModelForm):
     class Meta:
@@ -33,21 +33,108 @@ class CropForm(forms.ModelForm):
             }),
         }
 
-class FarmerRegistrationForm(UserCreationForm):
+class RegistrationForm(UserCreationForm):
+
+    USER_TYPES = (
+        ('farmer', 'Farmer'),
+        ('buyer', 'Buyer'),
+    )
+
+    user_type = forms.ChoiceField(
+        choices=USER_TYPES,
+        widget=forms.RadioSelect(attrs={'class': 'user-type-radio'})
+    )
+
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
         'class': 'form-input',
         'placeholder': 'Enter email address'
     }))
-    
+
+    phone = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'Phone Number'
+    }))
+
+    district = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'District'
+    }))
+
+    address = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-input',
+        'rows': 3,
+        'placeholder': 'Address'
+    }))
+
+    farm_size = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Farm Size'
+        })
+    )
+
+    main_crop = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Main Crop'
+        })
+    )
+
+    business_name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Business Name'
+        })
+    )
+
+    buying_interest = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Buying Interest'
+        })
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'email']
-        widgets = {
-            'username': forms.TextInput(attrs={
-                'class': 'form-input',
-                'placeholder': 'Enter username'
-            }),
-        }
+        fields = (
+            'username',
+            'email',
+            'password1',
+            'password2',
+            'user_type',
+            'phone',
+            'district',
+            'address',
+            'farm_size',
+            'main_crop',
+            'business_name',
+            'buying_interest',
+        )
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+            UserProfile.objects.create(
+                user=user,
+                user_type=self.cleaned_data['user_type'],
+                phone=self.cleaned_data['phone'],
+                district=self.cleaned_data['district'],
+                address=self.cleaned_data['address'],
+                farm_size=self.cleaned_data['farm_size'],
+                main_crop=self.cleaned_data['main_crop'],
+                business_name=self.cleaned_data['business_name'],
+                buying_interest=self.cleaned_data['buying_interest'],
+            )
+
+        return user
 
 class FarmerLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
