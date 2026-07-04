0686705d903e3cce95dcdfda787c65cd2ed7bba5
imtheking99 <kumarabp3@gmail.com>
2026-07-04 12:09:15 +0530

    accounts

--- agrilink/settings.py
+++ agrilink/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'Pages.apps.PagesConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
